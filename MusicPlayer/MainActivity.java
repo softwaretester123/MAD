2be33f3ed1969d8019f86161c8cacdb38be826e0
Ashwin Athappan <ashwinathappank@gmail.com>
2022-02-08 13:18:11 +0530

    Initial

--- MusicPlayer/MainActivity.java
+++ MusicPlayer/MainActivity.java
@@ -3,27 +3,31 @@ package com.example.music;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button start, stop;
-
+    MediaPlayer music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        music = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+
         start = findViewById(R.id.start);
         stop = findViewById(R.id.stop);
 
         start.setOnClickListener(v -> {
-            startService(new Intent(this, MusicService.class));
+            music.start();
         });
 
         stop.setOnClickListener(v -> {
-            stopService(new Intent(this, MusicService.class));
+            music.stop();
         });
     }
 }
